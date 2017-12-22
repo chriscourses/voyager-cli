2b04b09e5368a6d494405e49d1b70fa28cac652d
christopher4lis <christopher4lis@gmail.com>
2017-12-22 10:55:51 -0500

    Initial data commit.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: false,
+        es6: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2017
+    },
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never']
+    },
+    globals: {
+        module: true,
+        require: true,
+        __dirname: true,
+        process: true
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center"><img src="http://chriscourses.com/images/cc-voyager.svg"></p>
+
+## About Voyager CLI
+
+A Command Line Interface (CLI) for generating Voyager apps.
+
+``` bash
+$ npm install -g voyager-cli
+```
+
+### Usage
+
+``` bash
+$ voyager new <project-name>
+```
+
+Example:
+
+``` bash
+$ voyager new app
+```
+
+
+## License
+
+Voyager CLI is an open-source software running under the [MIT License](https://opensource.org/licenses/MIT).
